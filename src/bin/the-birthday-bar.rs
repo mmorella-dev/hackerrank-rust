1fdaa9c7f9a5671ae4fec24209f8d23dcebfaa9e
Mae B. Morella <4561733+mmorella-dev@users.noreply.github.com>
2024-03-19 13:50:11 -0400

    solution for "Subarray Division"

--- src/bin/the-birthday-bar.rs
+++ src/bin/the-birthday-bar.rs
@@ -1,19 +1,12 @@
+// https://www.hackerrank.com/challenges/the-birthday-bar/problem
 use std::env;
 use std::fs::File;
 use std::io::{self, BufRead, Write};
 
-/*
- * Complete the 'birthday' function below.
- *
- * The function is expected to return an INTEGER.
- * The function accepts following parameters:
- *  1. INTEGER_ARRAY s
- *  2. INTEGER d
- *  3. INTEGER m
- */
-
-fn birthday(s: &[i32], d: i32, m: i32) -> i32 {
-
+fn birthday(arr: &[i32], sum_to_match: i32, window_length: i32) -> i32 {
+    arr.windows(window_length as usize)
+        .filter(|&w| w.iter().sum::<i32>() == sum_to_match)
+        .count() as i32
 }
 
 fn main() {
@@ -22,15 +15,27 @@ fn main() {
 
     let mut fptr = File::create(env::var("OUTPUT_PATH").unwrap()).unwrap();
 
-    let n = stdin_iterator.next().unwrap().unwrap().trim().parse::<i32>().unwrap();
+    let _n = stdin_iterator
+        .next()
+        .unwrap()
+        .unwrap()
+        .trim()
+        .parse::<i32>()
+        .unwrap();
 
-    let s: Vec<i32> = stdin_iterator.next().unwrap().unwrap()
+    let s: Vec<i32> = stdin_iterator
+        .next()
+        .unwrap()
+        .unwrap()
         .trim_end()
         .split(' ')
         .map(|s| s.to_string().parse::<i32>().unwrap())
         .collect();
 
-    let first_multiple_input: Vec<String> = stdin_iterator.next().unwrap().unwrap()
+    let first_multiple_input: Vec<String> = stdin_iterator
+        .next()
+        .unwrap()
+        .unwrap()
         .split(' ')
         .map(|s| s.to_string())
         .collect();
@@ -42,4 +47,25 @@ fn main() {
     let result = birthday(&s, d, m);
 
     writeln!(&mut fptr, "{}", result).ok();
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::birthday;
+
+    #[test]
+    fn sample_input_0() {
+        let result = birthday(&[1, 2, 1, 3, 2], 3, 2);
+        assert_eq!(result, 2);
+    }
+    #[test]
+    fn sample_input_1() {
+        let result = birthday(&[1, 1, 1, 1, 1, 1], 3, 2);
+        assert_eq!(result, 0);
+    }
+    #[test]
+    fn sample_input_2() {
+        let result = birthday(&[4], 4, 1);
+        assert_eq!(result, 1);
+    }
 }
