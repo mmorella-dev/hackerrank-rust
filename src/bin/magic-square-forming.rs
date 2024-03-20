6e0b49151605718e70e57384af2ae7afcacf5985
Mae B. Morella <4561733+mmorella-dev@users.noreply.github.com>
2024-03-19 23:08:31 -0400

    Add sample code for "Magic Square Forming"

---
+++ src/bin/magic-square-forming.rs
@@ -0,0 +1,39 @@
+// https://www.hackerrank.com/challenges/magic-square-forming/problem?isFullScreen=true
+
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead, Write};
+
+/*
+ * Complete the 'formingMagicSquare' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY s as parameter.
+ */
+
+fn formingMagicSquare(s: &[Vec<i32>]) -> i32 {
+
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let mut stdin_iterator = stdin.lock().lines();
+
+    let mut fptr = File::create(env::var("OUTPUT_PATH").unwrap()).unwrap();
+
+    let mut s: Vec<Vec<i32>> = Vec::with_capacity(3_usize);
+
+    for i in 0..3_usize {
+        s.push(Vec::with_capacity(3_usize));
+
+        s[i] = stdin_iterator.next().unwrap().unwrap()
+            .trim_end()
+            .split(' ')
+            .map(|s| s.to_string().parse::<i32>().unwrap())
+            .collect();
+    }
+
+    let result = formingMagicSquare(&s);
+
+    writeln!(&mut fptr, "{}", result).ok();
+}
