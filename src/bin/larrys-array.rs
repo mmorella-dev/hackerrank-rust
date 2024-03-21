9a6a9f4eaba9c1f440c8144d74f9e191dda4331a
Mae B. Morella <4561733+mmorella-dev@users.noreply.github.com>
2024-03-20 23:05:42 -0400

    Add test cases for "Larry's Array"

--- src/bin/larrys-array.rs
+++ src/bin/larrys-array.rs
@@ -11,8 +11,8 @@ use std::io::{self, BufRead, Write};
  * The function accepts INTEGER_ARRAY A as parameter.
  */
 
-fn larrysArray(A: &[i32]) -> String {
-
+fn larrys_array(arr: &[i32]) -> bool {
+    todo!()
 }
 
 fn main() {
@@ -26,14 +26,26 @@ fn main() {
     for _ in 0..t {
         let n = stdin_iterator.next().unwrap().unwrap().trim().parse::<i32>().unwrap();
 
-        let A: Vec<i32> = stdin_iterator.next().unwrap().unwrap()
+        let a: Vec<i32> = stdin_iterator.next().unwrap().unwrap()
             .trim_end()
             .split(' ')
             .map(|s| s.to_string().parse::<i32>().unwrap())
             .collect();
 
-        let result = larrysArray(&A);
+        let result = if larrys_array(&a) { "YES" } else { "NO" };
 
         writeln!(&mut fptr, "{}", result).ok();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::larrys_array;
+
+    #[test]
+    fn sample_input_1() {
+        assert_eq!(larrys_array(&[3, 1, 2]), true);
+        assert_eq!(larrys_array(&[1, 3, 4, 2]), true);
+        assert_eq!(larrys_array(&[1, 2, 3, 5, 4]), false);
     }
 }
